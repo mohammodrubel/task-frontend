c7e2bf8732a25d1bb929207fb64121383230fd18
rubelrocks <md.rubel007@hotmail.com>
2024-04-03 17:48:58 +0600

    complete project .

--- src/App/api/baseApi.jsx
+++ src/App/api/baseApi.jsx
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {logout, setUser} from '../featchers/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl:'https://authintication-backend.vercel.app',
+    baseUrl:'https://authintication-backend.vercel.app/api/v1',
     credentials:'include',
     prepareHeaders:(headers,{getState})=>{
         const token = getState().auth.token
